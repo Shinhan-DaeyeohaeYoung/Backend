2601e0a8afb9e71ac3f1a73140ef408de2a5c632
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-08-26 10:59:43 +0900

    Fix: individualitem repo 수정

--- daeyo-domain/domain-rds/src/main/java/com/joeun/domain/item/repository/IndividualItemRepository.java
+++ daeyo-domain/domain-rds/src/main/java/com/joeun/domain/item/repository/IndividualItemRepository.java
@@ -2,6 +2,7 @@ package com.joeun.domain.item.repository;
 
 import com.joeun.domain.item.entity.*;
 import jakarta.persistence.LockModeType;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
@@ -11,11 +12,13 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface IndividualItemRepository extends  JpaRepository<IndividualItem, Long>{
+public interface IndividualItemRepository extends  JpaRepository<IndividualItem, Long> {
     Page<IndividualItem> findAllByItem(Item item, Pageable pageable);
+
     long countByItemAndStatus(Item item, IndividualItemStatus status);
 
     boolean existsByItemAndAssetNo(Item item, String assetNo);
+
     Optional<IndividualItem> findByItemAndAssetNo(Item item, String assetNo);
 
     long countByItem(Item item);
@@ -23,10 +26,13 @@ public interface IndividualItemRepository extends  JpaRepository<IndividualItem,
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<IndividualItem> findWithLockByItemAndAssetNo(Item item, String assetNo);
 
-    /** 유닛 행을 잠그고(비관적 락) 테넌트 경계로 조회 */
+    /**
+     * 유닛 행을 잠그고(비관적 락) 테넌트 경계로 조회
+     */
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("""
-      select u from IndividualItem u
+
+            select u from IndividualItem u
       where u.id = :unitId
         and u.item.universityId = :u
         and u.item.organizationId = :o
@@ -43,12 +49,19 @@ public interface IndividualItemRepository extends  JpaRepository<IndividualItem,
       """)
     Optional<IndividualItem> findByIdAndTenant(Long u, Long o, Long unitId);
 
+
     @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Transactional
     @Query("""
-      update IndividualItem i
-         set i.status = :toStatus
-       where i.id = :unitId
-         and i.status = :fromStatus
+        update IndividualItem i
+           set i.status = :toStatus
+         where i.id = :unitId
+           and i.status = :fromStatus
     """)
-    int updateStatusIfAvailable(Long unitId, IndividualItemStatus individualItemStatus, IndividualItemStatus individualItemStatus1);
-}
+    int updateStatusIfAvailable(
+            @Param("unitId") Long unitId,
+            @Param("fromStatus") IndividualItemStatus fromStatus,
+            @Param("toStatus") IndividualItemStatus toStatus
+    );
+
+    }
