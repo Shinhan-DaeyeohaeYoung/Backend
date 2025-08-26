136c3b4ec314c65e042c9a7dd9ea7772cbabeffe
DESKTOP-DLOPF19\SSAFY <sso9594@naver.com>
2025-08-26 10:40:47 +0900

    Feat: 홀딩 API 구현 및 security 설정 변경

---
+++ daeyo-domain/domain-rds/src/main/java/com/joeun/config/TransactionConfig.java
@@ -0,0 +1,15 @@
+package com.joeun.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class TransactionConfig {
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+}
