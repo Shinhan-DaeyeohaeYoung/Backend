c625f10c6225d80a7c6276ce472ae555da11f096
sso9594 <sso9594@naver.com>
2025-08-29 10:26:33 +0900

    Feat: cors 설정 추가

--- daeyo-external-api/src/main/java/com/joeun/global/config/WebConfig.java
+++ daeyo-external-api/src/main/java/com/joeun/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://frontend-five-flax-54.vercel.app")
                 .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
